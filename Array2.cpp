f43db1ddf7e9ffba6813a92a912fd3efc4af06e8
Sagar Manna <121675250+sagarmanna@users.noreply.github.com>
2024-09-15 23:34:34 +0530

    Add files via upload

---
+++ Array2.cpp
@@ -0,0 +1,13 @@
+
+
+//Intro to vector (c++ STL)
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+vector<int > v;
+cout<< v.size();
+    vector<int>v1(5,2);
+    cout<<"K";
+}
