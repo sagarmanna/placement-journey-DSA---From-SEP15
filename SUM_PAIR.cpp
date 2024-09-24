3954b4400afae60ff8ab01dbb319c5915ecc3075
Sagar Manna <121675250+sagarmanna@users.noreply.github.com>
2024-09-24 22:05:48 +0530

    Add files via upload

---
+++ SUM_PAIR.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int arr[]={1,2,3,4,5,6};
+int n = sizeof(arr)/sizeof(arr[0]);
+
+
+for(int i =0;i<n;i++){
+    for(int j=i+1;j<n;j++){
+        int sum_pair=arr[i]+arr[j];
+        cout<<sum_pair<<" ";
+    }
+}
+
+
+}
