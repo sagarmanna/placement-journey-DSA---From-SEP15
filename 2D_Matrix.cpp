13891ee651087a5fe9463cc7e7172c7de25d60d6
Sagar Manna <121675250+sagarmanna@users.noreply.github.com>
2024-10-14 21:29:18 +0530

    Add files via upload

--- 2D_Matrix.cpp
+++ 2D_Matrix.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
 
-int arr[4][4]={{1,2,3,4},{5,6,7,8},{4,8,9,8}};
+int arr[3][4]={{1,2,3,4},{5,6,7,8},{4,8,9,8}};
 int row =sizeof(arr)/sizeof(arr[0]);
 int col =sizeof(arr[0])/sizeof(arr[0][0]);
 
